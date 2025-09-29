9071c22cd4cccf7686bdb775962fc037b335153d
dev-mahbub <mahbub.devs@gmail.com>
2025-09-29 23:52:02 +0600

    product updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
